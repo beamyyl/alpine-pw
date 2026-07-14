025db3061982ec99f3db932e4beb54ceebefad86
beamyyl <128243705+beamyyl@users.noreply.github.com>
2026-07-14 19:19:09 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,31 +1,35 @@
 #!/bin/sh
-sudo setup-wayland-base
+
 sudo apk add pipewire wireplumber pipewire-pulse pipewire-alsa pipewire-jack pavucontrol dbus dbus-x11 util-linux-login
+
 sudo rc-update add dbus
 sudo rc-service dbus start
+
 sudo adduser "$USER" pipewire
+
 sudo cp -a /usr/share/pipewire /etc
 sudo cp -a /usr/share/wireplumber /etc
+
 rm -f "$HOME/.config/autostart/pipewire.desktop"
 rm -rf "$HOME/.config/pipewire/pipewire.conf.d"
 rm -f "$HOME/.config/service/pipewire" "$HOME/.config/service/wireplumber"
+
 sudo mkdir -p /etc/pipewire/pipewire.conf.d
+
 sudo tee /etc/pipewire/pipewire.conf.d/10-wireplumber.conf > /dev/null << 'EOF'
 context.exec = [
     { path = "/usr/bin/wireplumber" args = "" }
 ]
 EOF
+
 sudo tee /etc/pipewire/pipewire.conf.d/20-pipewire-pulse.conf > /dev/null << 'EOF'
 context.exec = [
     { path = "/usr/bin/pipewire-pulse" args = "" }
 ]
 EOF
-printf "Enter your choice (1 for Global XDG Autostart, 2 for Shell Profile): "
-read choice
-case $choice in
-    1)
-        sudo mkdir -p /etc/xdg/autostart
-        sudo tee /etc/xdg/autostart/pipewire.desktop > /dev/null << 'EOF'
+
+sudo mkdir -p /etc/xdg/autostart
+sudo tee /etc/xdg/autostart/pipewire.desktop > /dev/null << 'EOF'
 [Desktop Entry]
 Name=PipeWire
 Comment=Start PipeWire and its integrated services
@@ -34,21 +38,3 @@ Terminal=false
 Type=Application
 NoDisplay=true
 EOF
-        ;;
-    2)
-        if [ ! -f "$HOME/.profile" ]; then
-            touch "$HOME/.profile"
-        fi
-
-        if ! grep -q "pgrep -x \"pipewire\"" "$HOME/.profile" 2>/dev/null; then
-            cat << 'EOF' >> "$HOME/.profile"
-
-if ! pgrep -x "pipewire" > /dev/null; then
-    pipewire >/dev/null 2>&1 &
-fi
-EOF
-        fi
-        ;;
-    *)
-        ;;
-esac
