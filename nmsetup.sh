d57d3a8d5c7a41753bb45a4075594d909d218739
beamyyl <128243705+beamyyl@users.noreply.github.com>
2026-07-18 15:43:49 +0300

    Update nmsetup.sh

--- nmsetup.sh
+++ nmsetup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-sudo apk add networkmanager networkmanager-wifi networkmanager-tui network-manager-applet networkmanager-dev networkmanager-bluetooth dbus
+sudo apk add networkmanager networkmanager-wifi networkmanager-tui network-manager-applet networkmanager-dev networkmanager-bluetooth eudev dbus
 sudo addgroup -S plugdev 2>/dev/null
 sudo addgroup -S netdev 2>/dev/null
 sudo adduser "$USER" plugdev
@@ -29,7 +29,9 @@ sudo rc-service wpa_supplicant stop 2>/dev/null
 sudo rc-update del networking boot 2>/dev/null
 sudo rc-update del wpa_supplicant boot 2>/dev/null
 
+sudo rc-update add udev sysinit
 sudo rc-update add dbus boot
 sudo rc-update add networkmanager default
+sudo rc-service udev start
 sudo rc-service dbus start
 sudo rc-service networkmanager start
